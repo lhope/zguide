b05f903071de763a2356dfa430fda0b832163988
Luke Hope <lucas.r.hope@gmail.com>
2013-08-17 08:48:36 +1000

    cl-czmq: mtrelay

---
+++ examples/cl-czmq/mtrelay.lisp
@@ -0,0 +1,47 @@
+;;  Multithreaded relay
+
+(ql:quickload '("cl-czmq" "bordeaux-threads"))
+(use-package :cl-czmq)
+
+(defun step1 (context)
+  ;;  Connect to step2 and tell it we're ready
+  (with-zsockets context
+      ((xmitter :zmq-pair))
+    (zsocket-connect xmitter "inproc://step2")
+    (format t "Step 1 ready, signaling step 2~%")
+    (zstr-send xmitter "READY")))
+
+(defun step2 (context)
+  ;;  Bind inproc socket before starting step1
+  (with-zsockets context
+      ((receiver :zmq-pair))
+    (zsocket-bind receiver "inproc://step2")
+
+    (bordeaux-threads:make-thread
+     (lambda () (step1 context)))
+
+    ;;  Wait for signal and pass it on
+    (zstr-recv receiver))
+
+  ;;  Connect to step3 and tell it we're ready
+  (with-zsockets context
+      ((xmitter :zmq-pair))
+    (zsocket-connect xmitter  "inproc://step3")
+    (format t  "Step 2 ready, signaling step 3~%")
+    (zstr-send xmitter "READY")))
+
+(defun main ()
+  (with-zctx (context)
+    ;;  Bind inproc socket before starting step2
+    (with-zsockets context
+	((receiver :zmq-pair))
+      (zsocket-bind receiver "inproc://step3")
+
+      (bordeaux-threads:make-thread
+       (lambda () (step2 context)))
+
+      ;;  Wait for signal
+      (zstr-recv receiver))
+
+    (format t "Test successful!~%")
+    0))
